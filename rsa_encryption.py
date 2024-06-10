62320151790c32171bba2d37bbac58b3f2d0af36
Rick Rawson <rick.rawson@gmail.com>
2024-06-10 11:55:11 -0400

    -- modified encrypt_msg() and decrypt_msg() so that encrypt uses the recipient's public key and decrypt uses the recipient's private key -- left all commented-out code as-is

--- rsa_encryption.py
+++ rsa_encryption.py
@@ -17,10 +17,12 @@ Source: https://condor.depaul.edu/ichu/csc415/notes/notes4/rsa.html
 
 import json
 import math
+from pathlib import Path
 from random import randint
 
 import click
 from icecream import ic
+from pandas import qcut
 
 VERSION = "0.2"
 
@@ -46,7 +48,6 @@ def cli(message, file, printkeys, generate) -> None:
     # print()
     # ic(message, file, printkeys, generate)
     # print()
-
 
     main(message, file, printkeys, generate)
 
@@ -79,11 +80,16 @@ def encrypt_msg(msg) -> None:
 
     with open("sender.json", 'r', encoding='utf-8') as file:
         sender_keys = json.load(file)
+    with open("recipient.json", 'r', encoding='utf-8') as file:
+        recipient_keys = json.load(file)
 
-    e: int = sender_keys['public_key']['e']
-    n: int = sender_keys['public_key']['n']
+    e: int = recipient_keys['public_key']['e']
+    n: int = recipient_keys['public_key']['n']
+    # m: int = sender_keys['m']
+    # c: int = sender_keys['c']
 
     chunk_size: int = (n.bit_length() - 1) // 8  # Max bytes that n can handle minus a bit to be safe.
+    chunk_size=1 if chunk_size== 0 else chunk_size
 
     # Encode the message to bytes.
     message_bytes: bytes = msg.encode('utf-8')
@@ -91,10 +97,16 @@ def encrypt_msg(msg) -> None:
     # Convert the bytes to integers for encryption.
     e_msg: list[str] = []
     for i in range(0, len(message_bytes), chunk_size):
+
         chunk: bytes = message_bytes[i:i + chunk_size]
         chunk_int: int = int.from_bytes(chunk, byteorder='big')
-        ciphertext: str = str(pow(chunk_int, e, n))
-        e_msg.append(ciphertext)
+
+        # The following line computes c = (m**e) mod n behind the scenes.
+        # This means decryption needs "m".
+        ciphertext = chunk_int**e % n
+        # ciphertext: str = str(pow(chunk_int, e, n))
+
+        e_msg.append(str(ciphertext))
 
     encrypted_msg: str = " ".join(e_msg)
 
@@ -103,7 +115,7 @@ def encrypt_msg(msg) -> None:
         file.write(encrypted_msg)
 
 
-def decrypt_msg(private_key) -> str:
+def decrypt_msg() -> None:
     """
     Decrypt the contents of "encrypted.txt" using "d" & "n" in "private_key".
 
@@ -127,17 +139,27 @@ def decrypt_msg(private_key) -> str:
     p = Path("encrypted.txt")
     if p.exists():
         with open(p, "r", encoding='utf-8') as f:
-            msg: str = f.read()
+            m: str = f.read()
     else:
         print("\nencrypted.txt does not exist.")
         exit()
 
-    d: int = private_key[0]
-    n: int = private_key[1]
+    with open("sender.json", 'r', encoding='utf-8') as file:
+        sender_keys = json.load(file)
+    with open("recipient.json", 'r', encoding='utf-8') as file:
+        recipient_keys = json.load(file)
+
+    n: int = recipient_keys['public_key']['n']
+    e: int = recipient_keys['public_key']['e']
+    T: int = recipient_keys['public_key']['T']
+    # c: int = recipient_keys['c']
+    d: int = recipient_keys['private_key']
+    # m: int = (c**d) % n
 
     decrypted_chunks: list[bytes] = []
-    encrypted_chunks: list[int] = [int(x) for x in msg.split()]
+    encrypted_chunks: list[int] = [int(x) for x in m.split()]
     for ciphertext in encrypted_chunks:
+        decrypted_int = ciphertext**d % n
         decrypted_int: int = pow(ciphertext, d, n)
         chunk_size: int = (decrypted_int.bit_length() + 7) // 8
         decrypted_chunk: bytes = decrypted_int.to_bytes(chunk_size, byteorder='big')
@@ -151,7 +173,8 @@ def decrypt_msg(private_key) -> str:
         print("Access denied. Cannot decrypt with the provided key.")
         exit()
 
-    return decrypted_msg
+    with open('decrypted.txt', 'w', encoding='utf-8') as file:
+        file.write(decrypted_msg)
 
 
 def generate_keys():
@@ -187,57 +210,72 @@ def generate_keys():
         m = c**d % n = 13**27 mod 55 = 7
 
 
-    Integer values here are kept purposely small because there is no need rock-solid encryption, and using large numbers slows the processes of encrypt/decryption significantly.
+    CODENOTE:
+        Integer values here are kept purposely small because there is no need rock-solid encryption, and using large numbers slows the processes of encrypt/decryption significantly.
     """
 
     # round 1 creates the sender's keys and round 2 creates the recipient's keys.
     for round in range(1, 3):
 
-        if round == 1:  # the SENDER
-            n = 100
-            while n <= 1000:
-                p = 4
-                while not is_prime(p):
-                    p: int = randint(5, 100)
-                q = 4
-                while not is_prime(q):
-                    q: int = randint(4, p - 1)
-                n: int = p * q
-            p, q = sorted([p, q])
-            T: int = (p - 1) * (q - 1)   # T is for Totient
-            m: int = randint(10, 100) # m is a private number chosen by SENDER
+        # STEP 1:
+        #       SELECT TWO PRIME NUMBERS AND p < q
+        #       CREATE semi-prime "n" AS THE PRODUCT OF p * q
+        #       CREATE "T" AS THE TOTIENT
+        n = 100
+        # we are purposely keeping "n" below 1000
+        while n <= 1000:
+            p = 4
+            while not is_prime(p):
+                p: int = randint(5, 100)
+            q = 4
+            while not is_prime(q):
+                q: int = randint(4, p - 1)
+            n: int = p * q
+        p, q = sorted([p, q])
+        T: int = (p - 1) * (q - 1)   # T is for Totient
+        # m: int = randint(10, 100) # m is a private number chosen by SENDER
 
-            e, d = randint(5, 23), 3
-            while d <= e:
-                # e is a coprime of T: must be prime, < T, and must not be a factor of T
-                for e in range(max(p, q) + 1, T):
+        # STEP 2: CREATE "e", THE PUBLIC KEY
+        #       "e" MUST BE A PRIME
+        #       "e" < "T"
+        #       "e" MUST NOT BE A FACTOR OF "T"
+        for e in range(max(p, q) + 1, T-1):
 
-                    # Compute "d" since "d" must be less than or equal to "e".
-                    d: int = modinv(e, T)
+            # # Compute "d" since "d" must be less than or equal to "e".
+            # d: int = modinv(e, T)
 
-                    if is_prime(e) and coprime(e, T):
-                        break
+            # "e" is not a  factor of "T" is T % e != 0
+            f = T % e
 
-            c: int = (m**e) % n
-            sender_keys = {"public_key": {"n": n, "e": e, "T": T}, "c": c, "p": p, "q": q, "m": m}
-            with open("sender.json", 'w', encoding='utf-8') as file:
-                json.dump(sender_keys, file)
+            if is_prime(e) and e < T and f != 0:
+                # coprime(e, T):
+                break
 
-        else:  # the RECIPIENT
-            with open("sender.json", "r", encoding='utf-8') as file:
-                sender_keys: dict[str, int] = json.load(file)
-            n: int = sender_keys['public_key']['n']
-            e: int = sender_keys['public_key']['e']
-            T: int = sender_keys['public_key']['T']
-            c: int = sender_keys['c']
+        # STEP 3: CREATE A PRIVATE KEY "d" such that (d*e) % T == 1
+        #       this requires using a multiplicative inverse function
+        d: int = modinv(e, T)
 
-            # reconstruct "m"
-            d = modinv(e, T)
-            m = (c**d) % n
+        # c: int = (m**e) % n
 
-            recipient_keys: dict[str, int]  = {"public_key": {"n": n, "e": e, "T": T}, "c": c, "p": p, "q": q, "m": m}
-            with open("recipient.json", 'w', encoding='utf-8') as file:
-                json.dump(recipient_keys, file)
+        keys = {"public_key": {"n": n, "e": e, "T": T}, "private_key": d, "p": p, "q": q}
+        filename = "sender.json" if round == 1 else "recipient.json"
+        with open(filename, 'w', encoding='utf-8') as file:
+                json.dump(keys, file)
+
+        # else:  # the RECIPIENT
+        #     with open("sender.json", "r", encoding='utf-8') as file:
+        #         sender_keys: dict[str, int] = json.load(file)
+        #     n: int = sender_keys['public_key']['n']
+        #     e: int = sender_keys['public_key']['e']
+        #     T: int = sender_keys['public_key']['T']
+        #     c: int = sender_keys['c']
+
+        #     # reconstruct "m"
+        #     d = modinv(e, T)
+
+        #     recipient_keys: dict[str, int]  = {"public_key": {"n": n, "e": e, "T": T}, "c": c, "p": p, "q": q}
+        #     with open("recipient.json", 'w', encoding='utf-8') as file:
+        #         json.dump(recipient_keys, file)
 
 
 # ==== UTILITY FUNCTIONS =====================================================
@@ -421,12 +459,12 @@ def main(msg: str, file: str, printkeys: str, generate: str) -> None:
         print('Encrypted message saved as "encrypted.txt".')
 
     else:
-        recipient: str = input("Who is the recipient of this message/file: ").lower()
-        keys = get_keys(recipient)
-        decrypted_msg: str = decrypt_msg(keys['private_key'])
-        with open('decrypted.txt', 'w', encoding='utf-8') as f:
-            f.write(decrypted_msg)
-
+        # recipient: str = input("Who is the recipient of this message/file: ").lower()
+        # keys = get_keys(recipient)
+        # decrypted_msg: str = decrypt_msg(keys['private_key'])
+        # with open('decrypted.txt', 'w', encoding='utf-8') as f:
+        #     f.write(decrypted_msg)
+        decrypt_msg()
         print('Decrypted message saved as "decrypted.txt"')
 
 
